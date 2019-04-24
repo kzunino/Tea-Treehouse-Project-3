08e665c397f73815fca360433f4eb4bafdd4372c
Kyle <kzunino@gmail.com>
2019-04-24 13:45:58 -0700

    refactoring BB

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ const submitButton = $('button[type="submit"]');
 
 const validatorSpans = $('.validator');                                   //creates validator variable to hide validation error messages
 
-validatorSpans.hide();                                                    //hides the error messages
+validatorSpans.hide();                                                    //hides the error messages when page loads
 
 let activityCosts = $('[name="total"]').val(0);                          //sets the start value of activity costs to 0 because no workships are selected
 
@@ -101,30 +101,45 @@ design.on('click', function (){                     // function to hide/show col
 
 // ***** ”Register for Activities” section *******
 
+function enableCheckbox(element){
+    element.prop('disabled', false).show();
+}
+function disableCheckbox(element){
+  element.prop('disabled', true).hide();
+}
+
 checkBoxCollection.on('change', function(){         //fucntion to disable conflicting times for workshops
   if (js_frameworks.is(':checked')){                //if js_frameworks is checked then express workship is disabled and checkbox is hidden
-    express.prop('disabled', true).hide();
+    //express.prop('disabled', true).hide();
+    disableCheckbox(express);
   }
   if (js_frameworks.is(':checked') === false){        //if js_frameworks is unchecked then express.prop is reenabled and clickable
-    express.prop('disabled', false).show();
+    //express.prop('disabled', false).show();
+    enableCheckbox(express);
   }
   if (express.is(':checked')){
-    js_frameworks.prop('disabled', true).hide();
+    //js_frameworks.prop('disabled', true).hide();
+    disableCheckbox(js_frameworks);
   }
   if (express.is(':checked') === false){
-    js_frameworks.prop('disabled', false).show();
+    //js_frameworks.prop('disabled', false).show();
+    enableCheckbox(js_frameworks);
   }
   if (js_libs.is(':checked')){
-    node.prop('disabled', true).hide();
+    //node.prop('disabled', true).hide();
+    disableCheckbox(node);
   }
   if (js_libs.is(':checked') === false){
-    node.prop('disabled', false).show();
+    //node.prop('disabled', false).show();
+    enableCheckbox(node);
   }
   if (node.is(':checked')){
-    js_libs.prop('disabled', true).hide();
+    //js_libs.prop('disabled', true).hide();
+    disableCheckbox(js_libs);
   }
   if (node.is(':checked') === false){
-    js_libs.prop('disabled', false).show();
+    //js_libs.prop('disabled', false).show();
+    enableCheckbox(js_libs);
   }
 });
 
